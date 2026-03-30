9a4a3094333b5b80e723f874720780329212a651
daehyuk1231 <daehyuk1231@naver.com>
2026-03-30 10:24:18 +0900

    변수와 타입

---
+++ java/ch02/Variables.java
@@ -0,0 +1,17 @@
+package ch02;
+
+public class Variables {
+    public static void main(String[] args) {
+        //정수형 변수
+        int age = 10;
+
+        //실수형 변수
+        double height = 175.5f;
+
+        //논리형 변수
+        boolean isStudent = true;
+
+        //문자열 변수
+        String name = "Alice";
+    }
+}
