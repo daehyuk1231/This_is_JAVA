f00db3f9bde051674cd83d55e166278d45c50988
daehyuk1231 <daehyuk1231@naver.com>
2026-03-30 16:08:17 +0900

    예제

--- java/ch01/ch06/Greet.java
+++
@@ -1,7 +0,0 @@
-package ch01.ch06;
-
-public class Greet {
-    public static void main(String[] args) {
-        System.out.println("Greetings!");
-    }
-}
--- java/ch01/ch06/Hello.java
+++
@@ -1,7 +0,0 @@
-package ch01.ch06;
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World1!");
-        System.out.println("Hello, World2!");
-    }
-}
