e088f69619a94950b5f3c409752682bcf54a2ff1
daehyuk1231 <daehyuk1231@naver.com>
2026-03-30 12:36:44 +0900

    for 문

--- java/ch04/sec04/PrintFromTo10Example.java
+++ java/ch04/sec04/PrintFromTo10Example.java
@@ -2,8 +2,12 @@ package ch04.sec04;
 
 public class PrintFromTo10Example {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i + " ");
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            }
         }
+        System.out.println("1부터 100까지 3의 배수의 합: " + sum);
     }
 }
